5b318a5cd0c80764a141a62b3ef2c6bc35d90a6b
Kristofer <kris@youngers.org>
2024-09-24 16:04:52 -0400

    re-start with phx.gen.auth

--- zdocs/lib/zdocs/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Zdocs.Mailer do
-  use Swoosh.Mailer, otp_app: :zdocs
-end
--- zdocs/lib/zdocs/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Zdocs.Repo do
-  use Ecto.Repo,
-    otp_app: :zdocs,
-    adapter: Ecto.Adapters.Postgres
-end
